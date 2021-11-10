14120afad9ddd1cfa0fc024202927587b2266428
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-10 10:22:47 +0000

    startying task 2

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+console.log('I am having a painful day since i cant feel my shoulder or legs because of kickboxing last night :£|')
+
+function sayHello(name) {
+    return `Hello ${name}`
+}
+
+console.log(sayHello('BBy Girl'))
+
+console.log('Startying task 2')
+
